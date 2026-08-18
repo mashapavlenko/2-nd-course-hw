00814a6f808b378958eb616b590af723ffd72e65
mashapavlenko <pavlenkomaria046@gmail.com>
2026-08-18 21:24:21 +0400

    Added functional to the games in site.js

--- site.js
+++ site.js
@@ -29,3 +29,55 @@ for (let i=0; i < quiz.length; i++){
 alert('Кол-во правильных ответов: ' + correct);
     
 }
+// Игра "Угадай число"
+function guessNum(){
+    let num = Math.floor(Math.random() * 100) + 1;
+    let question;
+    while (question != num) {
+        question = Number(prompt('Угадайте число от 1 до 100'));
+        if (question === num){
+            alert('Угадал!');
+        } else if (question < num){
+            alert('Загаданное число больше');
+        } else {
+            alert('Загаданное число меньше');
+        }
+    }
+}
+// Игра "Простая арифметика"
+function arithmetic(){
+    let a = Math.floor(Math.random() * 20) +1;
+    let b = Math.floor(Math.random() * 20) +1;
+
+    let operation = Math.floor(Math.random() * 4) + 1; 
+    let correctResponse;
+    let userAnswer;
+    if (operation === 1) {
+        correctResponse = a + b;
+        userAnswer = Number(prompt(a + '+' + b));
+    } else if (operation ===2){
+        correctResponse = a - b;
+        userAnswer = Number(prompt(a + '-' + b));
+    }else if(operation === 3){
+        correctResponse = a * b;
+        userAnswer = Number(prompt(a + '*' + b));
+    }else{
+        correctResponse = a / b;
+        userAnswer = Number(prompt(a + '/' + b));
+    }
+    
+    if (userAnswer === correctResponse){
+        alert('Верно!');
+    }else{
+        alert('Ошибка!');
+    }
+}
+
+// Игра "Переверни текст"
+function reverso(){
+    let userQuestion = prompt("Введите текст");
+    userQuestion = userQuestion.split('').reverse().join('');
+
+    alert (userQuestion);
+}
+
